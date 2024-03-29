aaac9955eb470a3c4a4ca9747e37e30028cc866c
Alex Meyer <alexwreyem@gmail.com>
2024-03-28 20:14:48 -0400

    fix/tweak flatpak stuff

--- flatpaks.sh
+++ flatpaks.sh
@@ -2,12 +2,16 @@
 
 set -ouex pipefail
 
+INSTALL_FILE_DIR="/usr/share/ublue-os/flatpaks"
+INSTALL_FILE="$INSTALL_FILE_DIR/install"
+
+mkdir -p "$INSTALL_FILE_DIR"
+rm -f "$INSTALL_FILE"
+
 # exit if this image is in "skip" array
 jq -e ".skip | index(\"$IMAGE_NAME\")" /tmp/flatpaks.json >/dev/null && exit 0
 
-INSTALL_FILE_DIR="/usr/share/ublue-os/flatpaks"
-INSTALL_FILE="$INSTALL_FILE_DIR/install"
-mkdir -p "$INSTALL_FILE_DIR"
+systemctl enable -f system-flatpak-setup.service
 
 # combine "all" and "$IMAGE_NAME" entries, subtracting excluded ones
 jq -r "[(.include | (select(.all != null).all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] - \
