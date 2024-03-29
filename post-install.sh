92d188836e55357d19613d43c91d30aa760ac9ad
Alex Meyer <alexwreyem@gmail.com>
2024-03-28 20:14:31 -0400

    make zsh default for new users

--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,5 @@ set -ouex pipefail
 systemctl enable sshd.service
 systemctl enable libvirtd.service
 systemctl enable tailscaled.service
+
+sed -i 's@SHELL=.*@SHELL=/usr/bin/zsh@' /etc/default/useradd
