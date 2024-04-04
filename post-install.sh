152e0d5bbfb4c09f74d3b2e2457aa8cbd52df219
Alex Meyer <alexwreyem@gmail.com>
2024-04-04 08:44:23 -0400

    make libvirtd service enable optional

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,8 @@
 set -ouex pipefail
 
 systemctl enable sshd.service
-systemctl enable libvirtd.service
 systemctl enable tailscaled.service
+
+systemctl enable libvirtd.service 2>/dev/null || true
 
 sed -i 's@SHELL=.*@SHELL=/usr/bin/zsh@' /etc/default/useradd
