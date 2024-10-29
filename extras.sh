7b95b68b8c873b41dd46abc4e8687a404bff9f10
Alex Meyer <alexwreyem@gmail.com>
2024-10-29 14:45:00 -0400

    new keyboard shortcut method, alacritty .desktop

--- extras.sh
+++ extras.sh
@@ -41,4 +41,19 @@ if [[ -d /usr/share/plasma ]]; then # only run on KDE images
   kpackagetool6 --type=KWin/Script -g -i "$TMPDIR/switch-to-previous-desktop-master/package"
   
   rm -rf "$TMPFILE" "$TMPDIR"
+
+  # change default keybinds
+  sed -i 's/\(X-KDE-Shortcuts=.*\),Meta+Shift+S/\1/g' /usr/share/kglobalaccel/org.kde.spectacle.desktop
+  sed -i '/\[Desktop Action RectangularRegionScreenShot\]/,/^\[/ s/\(X-KDE-Shortcuts=.*\)/\1,Meta+Shift+S/g' /usr/share/kglobalaccel/org.kde.spectacle.desktop
+
+  if [[ -e /usr/bin/alacritty ]]; then
+    # copy default .desktop file to make a distrobox one, change settings and add KDE shortcuts
+    cp /usr/share/applications/Alacritty.desktop /usr/share/applications/Alacritty-distrobox.desktop
+    sed -i 's/\[Desktop Action New\]/\[Desktop Action New\]\nX-KDE-Shortcuts=Meta+Shift+Return/g' /usr/share/applications/Alacritty.desktop
+    sed -i 's/\[Desktop Action New\]/\[Desktop Action New\]\nX-KDE-Shortcuts=Meta+Return/g' /usr/share/applications/Alacritty-distrobox.desktop
+    sed -i 's/^Exec=alacritty/Exec=alacritty -T "Alacritty \(distrobox\)" --class "Alacritty \(distrobox\)" -e distrobox-enter/g' /usr/share/applications/Alacritty-distrobox.desktop
+    sed -i 's/^\(Name\|StartupWMClass\)=Alacritty/\1=Alacritty \(distrobox\)/g' /usr/share/applications/Alacritty-distrobox.desktop
+    ln -s /usr/share/applications/Alacritty.desktop /usr/share/kglobalaccel/Alacritty.desktop
+    ln -s /usr/share/applications/Alacritty-distrobox.desktop /usr/share/kglobalaccel/Alacritty-distrobox.desktop
+  fi  
 fi
