bc5499c429753254cccae703e8cdf739ab0f995b
Alex Meyer <alexwreyem@gmail.com>
2025-05-01 21:17:42 -0400

    no sudo for you

--- build_scripts/21-packages-misc.sh
+++ build_scripts/21-packages-misc.sh
@@ -7,9 +7,9 @@ TMPDIR="$(mktemp -d)"
 
 cd "$TMPDIR"
 wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
-sudo chmod +x eza
-sudo chown root:root eza
-sudo mv eza /usr/bin/eza
+chmod +x eza
+chown root:root eza
+mv eza /usr/bin/eza
 
 git clone https://github.com/eza-community/eza.git
 mv eza/completions/zsh/* /usr/share/zsh/site-functions/
