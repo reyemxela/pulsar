e4a57b312d20f4ca905d0aa3b099ac6fbb44bf16
Alex Meyer <alexwreyem@gmail.com>
2025-07-12 00:29:53 -0400

    add podlet to packages-misc

--- build_scripts/21-packages-misc.sh
+++ build_scripts/21-packages-misc.sh
@@ -17,3 +17,15 @@ mv eza/completions/bash/* /usr/share/bash-completion/completions/
 
 cd /
 rm -rf "$TMPDIR"
+
+
+# podlet
+TMPDIR="$(mktemp -d)"
+
+cd "$TMPDIR"
+wget -c https://github.com/containers/podlet/releases/latest/download/podlet-x86_64-unknown-linux-gnu.tar.xz -O - | tar xJ --strip-components=1
+chmod +x podlet
+chown root:root podlet
+mv podlet /usr/bin/podlet
+cd /
+rm -rf "$TMPDIR"
