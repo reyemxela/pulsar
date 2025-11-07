3b1dc3560d883b8aeffdf8eef5f03244a569d1ec
Alex Meyer <alexwreyem@gmail.com>
2025-11-07 16:30:31 -0500

    fix netbird service file

--- build_scripts/25-package-tweaks.sh
+++ build_scripts/25-package-tweaks.sh
@@ -4,3 +4,8 @@ set -ouex pipefail
 
 # docker zsh completions
 docker completion zsh >/usr/share/zsh/site-functions/_docker
+
+
+# fix netbird /var/log issues
+# https://github.com/netbirdio/netbird/issues/3866#issuecomment-3504960886
+sed -i '/^Standard\(Output\|Error\)=/d' /etc/systemd/system/netbird.service
